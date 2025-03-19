b0cc97b81788c8f79bfdc41702996e1eb087aff6
Sumedh <56512410+sumedh27@users.noreply.github.com>
2025-03-19 07:12:09 +0000

    Add working create todos in console

--- src/index.js
+++ src/index.js
@@ -1,19 +1,62 @@
 import styles from "./styles.css"
 
+const allTodos = [
+    { 
+        title: `Test`,
+        description: `This is a test`,
+        dueDate: `22-02-2025`,
+        priority: `high`,
+        checklist: false
+    },
+    {
+        title: `Test2`,
+        description: `This is a SECOND TEST`,
+        dueDate: `22-02-2026`,
+        priority: `low`,
+        checklist: false
+    },
+    {
+        title: `Test3`,
+        description: `This is a THIRD TEST`,
+        dueDate: `22-02-2025`,
+        priority: `high`,
+        checklist: false
+    }
+];
+
+const getTodos = () => allTodos;
+
+
 const createTodo = function (title,description,dueDate,priority,checklist) {
 
-    const todo = [
+    const todo =
         {
             title: title,
             description: description,
             dueDate: dueDate,
             priority: priority,
             checklist: checklist
-        }
-    ]
+        };
 
-    return todo;
+    allTodos.push(todo);
+    return allTodos;
 };
+
+createTodo(`THIS IS A NEW TEST`,`THIS IS NEW DESCRIPTION LULW`,`23-12-2004`,`low`,true);
+createTodo(`THIS IS A NEW TEST`,`THIS IS NEW DESCRIPTION LULW`,`23-12-2004`,`low`,true);
+createTodo(`THIS IS A NEW TEST`,`THIS IS NEW DESCRIPTION LULW`,`23-12-2004`,`low`,true);
+
+
+console.log(getTodos());
+
+const Todos = (function(){
+    const printTodos = getTodos();
+
+    return printTodos.forEach((todo,index) => {
+        console.log(`Todo: ${index + 1} Title: ${todo.title}`);
+    });
+
+})();
 
 
 const allProjects = [        
@@ -34,7 +77,7 @@ const createProject = function(name,description){
         };
 
     allProjects.push(project);
-    
+
     return allProjects;
 };
 
@@ -42,7 +85,7 @@ createProject(`test`,`testasdasdasd`);
 createProject(`tes1t`,`testasdasdasd`);
 createProject(`tes2t`,`testasdasdasd`);
 
-
+console.log(getProjects());
 
 const Projects = (function(){
     const printProjects = getProjects();
