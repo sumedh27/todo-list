178fd51a08124f3d7fc4d0785f8df73e8ff3312b
Sumedh <56512410+sumedh27@users.noreply.github.com>
2025-03-19 07:03:18 +0000

    Add working Projects in console

--- src/index.js
+++ src/index.js
@@ -1,3 +1,59 @@
 import styles from "./styles.css"
 
-console.log(`Test`);
+const createTodo = function (title,description,dueDate,priority,checklist) {
+
+    const todo = [
+        {
+            title: title,
+            description: description,
+            dueDate: dueDate,
+            priority: priority,
+            checklist: checklist
+        }
+    ]
+
+    return todo;
+};
+
+
+const allProjects = [        
+    { 
+        name: `Test`,
+        description: `This is a test`,
+    }
+];
+
+const getProjects = () => allProjects;
+
+
+const createProject = function(name,description){
+    const project = 
+        {
+            name: name,
+            description: description
+        };
+
+    allProjects.push(project);
+    
+    return allProjects;
+};
+
+createProject(`test`,`testasdasdasd`);
+createProject(`tes1t`,`testasdasdasd`);
+createProject(`tes2t`,`testasdasdasd`);
+
+
+
+const Projects = (function(){
+    const printProjects = getProjects();
+
+    return printProjects.forEach((project,index) => {
+        console.log(`Project Number: ${index + 1} Project Name: ${project.name}`);
+    });
+
+})();
+
+
+
+
+
